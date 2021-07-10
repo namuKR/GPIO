cae8084596469d6fc6675a253ff138d331825bb8
namuKR <desbey1@gmail.com>
2021-07-10 11:51:14 +0900

    LED BLUE

--- 001_LED.py
+++ 001_LED.py
@@ -3,24 +3,32 @@ import time
 ### CONFIG BCM PORTS ###
 LED_RED = 17
 LED_YELLOW = 27
+LED_BLUE = 22
 ########################
 GPIO.setmode(GPIO.BCM)
 ########################
 ### SETUP LED TO OUT ###
 GPIO.setup(LED_RED,GPIO.OUT)
 GPIO.setup(LED_YELLOW,GPIO.OUT)
+GPIO.setup(LED_BLUE, GPIO.OUT)
 ########################
 # INPUT #
-sec = int(input('Enter Interval'))
+sec = float(input('Enter Interval'))
 #########
 ## BLINK ##
 try:
     while True:
         GPIO.output(LED_RED, GPIO.HIGH)
+        time.sleep(sec)
         GPIO.output(LED_YELLOW, GPIO.HIGH)
         time.sleep(sec)
-        GPIO.output(LED_RED, GPIO.LOW)
+        GPIO.output(LED_BLUE, GPIO.HIGH)
+        time.sleep(sec)
+        GPIO.output(LED_BLUE, GPIO.LOW)
+        time.sleep(sec)
         GPIO.output(LED_YELLOW, GPIO.LOW)
+        time.sleep(sec)
+        GPIO.output(LED_RED, GPIO.LOW)
         time.sleep(sec)
 except KeyboardInterrupt:
     GPIO.cleanup()
